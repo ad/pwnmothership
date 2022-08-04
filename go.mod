a95e7aac6309ef1b5bac70f674e3137bc6b1c911
Daniel Apatin <daniel@apatin.ru>
2022-08-04 14:13:05 +0300

    updated to Go 1.18 and "embed" (replaced statik)

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/ad/pwnmothership
 
-go 1.14
-
-require github.com/rakyll/statik v0.1.7
+go 1.18
--- main.go
+++ main.go
@@ -3,24 +3,24 @@
 package main
 
 import (
-	"flag"
+	"embed"
 	"encoding/json"
+	"flag"
+	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
 	"time"
-
-	st "github.com/ad/pwnmothership/static"
-
-	// go get github.com/rakyll/statik
-	_ "github.com/ad/pwnmothership/statik"
 )
 
 var (
-	addr   *string
-	static *st.FS
-	p = make(map[string]*Pwnagotchi)
+	addr *string
+	p    = make(map[string]*Pwnagotchi)
+
+	//go:embed public
+	public embed.FS
 )
 
 // Pwnagotchi ...
@@ -71,133 +71,129 @@ type Pwnagotchi struct {
 func main() {
 	addr = flag.String("addr", ":8080", "listen address")
 	flag.Parse()
-	
-	static := st.NewFS()
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(static.StatikFS)))
+	http.Handle("/static/css/", http.StripPrefix("/static/css/", fsHandler("css")))
+	http.Handle("/static/favicon/", http.StripPrefix("/static/favicon/", fsHandler("favicon")))
+	http.Handle("/static/font/", http.StripPrefix("/static/font/", fsHandler("font")))
+	http.Handle("/static/js/", http.StripPrefix("/static/js/", fsHandler("js")))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		path := r.URL.Path
-		if path == "/" {
-			w.Header().Set("Content-Type", "text/html")
-			path = "html/default.html"
-		}
-		if path == "/apple-touch-icon.png" || path == "/favicon-32x32.png" || path == "/favicon-16x16.png" || path == "/favicon.ico" {
-			path = "favicon" + path
-		}
-
-		if content, err := static.ReadFile(path); err == nil {
-			w.Write(content)
-		} else {
-			http.NotFound(w, r)
-			log.Println(path, err)
-		}
-	})
-
-	http.HandleFunc("/api/get", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			http.NotFound(w, r)
-			return
-		}
-
-		hash := r.URL.Query().Get("hash")
-
-		w.Header().Set("Content-Type", "application/json")
-
-		if hash == "test" {
-			d := &Pwnagotchi{
-				Fingerprint: "",
-				Initialised: true,
-
-				Name:   randomString([]string{"pwnagotchi", "username", "test", "pet", "cat", "dog"}),
-				Mode:   randomString([]string{"AUTO", "MANU", "AI"}),
-				Status: randomString([]string{"...", "ololo", "blabla", "Связываюсь с 2020", "Эй, 2020 давай дружить!", "Ждем 5c …", "Хррррр.. (29c)", "Дремлет 19с …", "Осматриваюсь вокруг (3с)", "Просто решил, что 00:12:34:56:78:0a не нужен WiFi! Кхе-кхе)"}),
-
-				Uptime:  randomString([]string{"00:00:01", "12:34:56", "23:45:00"}),
-				Version: "1.5.3",
-				Epoch:   randomInt64([]int64{1, 2, 3, 10, 20, 300}),
-
-				APsOnChannel: randomInt64([]int64{1, 2, 3, 10, 20, 300}),
-				APsName:      randomString([]string{"1 (1)", "2 (5)", "10 (100)"}),
-				APsTotal:     randomInt64([]int64{1, 2, 3, 10, 20, 300}),
-				Channel:      randomInt64([]int64{1, 2, 3, 10, 20, 300}),
-				ChannelName:  randomString([]string{"*", "1", "11"}),
-
-				Face: randomString([]string{"(⇀‿‿↼)", "(≖‿‿≖)", "(◕‿‿◕)", "( ⚆_⚆)", "(☉_☉ )", "( ◕‿◕)", "(◕‿◕ )", "(°▃▃°)", "(⌐■_■)", "(•‿‿•)", "(^‿‿^)", "(ᵔ◡◡ᵔ)", "(✜‿‿✜)", "(♥‿‿♥)", "(☼‿‿☼)", "(≖__≖)", "(-__-)", "(╥☁╥ )", "(ب__ب)", "(☓‿‿☓)", "(#__#)"}),
-
-				FriendFace: randomString([]string{"(⇀‿‿↼)", "(≖‿‿≖)", "(◕‿‿◕)", "( ⚆_⚆)", "(☉_☉ )", "( ◕‿◕)", "(◕‿◕ )", "(°▃▃°)", "(⌐■_■)", "(•‿‿•)", "(^‿‿^)", "(ᵔ◡◡ᵔ)", "(✜‿‿✜)", "(♥‿‿♥)", "(☼‿‿☼)", "(≖__≖)", "(-__-)", "(╥☁╥ )", "(ب__ب)", "(☓‿‿☓)", "(#__#)"}),
-				FriendName: randomString([]string{"username", "test", "pet"}),
-
-				CPU:         0.5,
-				Memory:      0.5,
-				Temperature: 35.5,
-				Bluetooth:   randomString([]string{"-", "C", "NF"}),
-				UPS:         randomString([]string{"", "0%", "1%", "10%", "11%", "100%"}),
-
-				PeersCount: randomInt64([]int64{1, 2, 3, 10, 20, 300}),
-
-				PwnedLast:   randomString([]string{"pwnagotchi", "username", "test", "pet", "cat", "dog"}),
-				PwnedRun:    "2",
-				PwnedTotal:  randomInt64([]int64{1, 2, 3, 10, 20, 300}),
-				PwnedDeauth: "4",
-
-				TotalMessages:  randomInt64([]int64{1, 2, 3, 10, 20, 300}),
-				UnreadMessages: randomInt64([]int64{1, 2, 3, 10, 20, 300}),
-
-				Level: randomString([]string{"-", "1", "11", "50", "99"}),
-				Exp:   randomString([]string{"╷          ╷", "╷▄         ╷", "╷▄▄        ╷", "╷▄▄▄       ╷", "╷▄▄▄▄      ╷", "╷▄▄▄▄▄     ╷", "╷▄▄▄▄▄▄    ╷", "╷▄▄▄▄▄▄▄   ╷", "╷▄▄▄▄▄▄▄▄  ╷", "╷▄▄▄▄▄▄▄▄▄▄╷"}),
-			}
-
-			b, _ := json.Marshal(d)
-			w.Write(b)
-			return
-		}
-
-		notFound := `{"initialized": false}`
-
-		if d, ok := p[hash]; ok {
-			b, err := json.Marshal(d)
-			if err != nil {
-				w.Write([]byte(notFound))
-				return
-			}
-
-			w.Write(b)
-			return
-		}
-
-		w.Write([]byte(notFound))
-	})
-
-	http.HandleFunc("/api/set", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.NotFound(w, r)
-			return
-		}
-
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Println(err)
-		}
-
-		var d Pwnagotchi
-		if err := json.Unmarshal(body, &d); err != nil {
-			log.Println(err)
-		} else {
-			if d.Fingerprint != "" {
-				p[d.Fingerprint] = &d
-			}
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte(`{"success": true}`))
-	})
+	http.Handle("/", fsHandler("html"))
+	http.HandleFunc("/api/get", apiGet)
+	http.HandleFunc("/api/set", apiSet)
 
 	log.Println("start listening on", *addr)
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
+}
+
+func fsHandler(subPath string) http.Handler {
+	sub, err := fs.Sub(public, "public/"+subPath)
+	if err != nil {
+		fmt.Println("file not found", err)
+	}
+
+	return http.FileServer(http.FS(sub))
+}
+
+func apiGet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.NotFound(w, r)
+		return
+	}
+
+	hash := r.URL.Query().Get("hash")
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if hash == "test" {
+		d := &Pwnagotchi{
+			Fingerprint: "",
+			Initialised: true,
+
+			Name:   randomString([]string{"pwnagotchi", "username", "test", "pet", "cat", "dog"}),
+			Mode:   randomString([]string{"AUTO", "MANU", "AI"}),
+			Status: randomString([]string{"...", "ololo", "blabla", "Связываюсь с 2020", "Эй, 2020 давай дружить!", "Ждем 5c …", "Хррррр.. (29c)", "Дремлет 19с …", "Осматриваюсь вокруг (3с)", "Просто решил, что 00:12:34:56:78:0a не нужен WiFi! Кхе-кхе)"}),
+
+			Uptime:  randomString([]string{"00:00:01", "12:34:56", "23:45:00"}),
+			Version: "1.5.3",
+			Epoch:   randomInt64([]int64{1, 2, 3, 10, 20, 300}),
+
+			APsOnChannel: randomInt64([]int64{1, 2, 3, 10, 20, 300}),
+			APsName:      randomString([]string{"1 (1)", "2 (5)", "10 (100)"}),
+			APsTotal:     randomInt64([]int64{1, 2, 3, 10, 20, 300}),
+			Channel:      randomInt64([]int64{1, 2, 3, 10, 20, 300}),
+			ChannelName:  randomString([]string{"*", "1", "11"}),
+
+			Face: randomString([]string{"(⇀‿‿↼)", "(≖‿‿≖)", "(◕‿‿◕)", "( ⚆_⚆)", "(☉_☉ )", "( ◕‿◕)", "(◕‿◕ )", "(°▃▃°)", "(⌐■_■)", "(•‿‿•)", "(^‿‿^)", "(ᵔ◡◡ᵔ)", "(✜‿‿✜)", "(♥‿‿♥)", "(☼‿‿☼)", "(≖__≖)", "(-__-)", "(╥☁╥ )", "(ب__ب)", "(☓‿‿☓)", "(#__#)"}),
+
+			FriendFace: randomString([]string{"(⇀‿‿↼)", "(≖‿‿≖)", "(◕‿‿◕)", "( ⚆_⚆)", "(☉_☉ )", "( ◕‿◕)", "(◕‿◕ )", "(°▃▃°)", "(⌐■_■)", "(•‿‿•)", "(^‿‿^)", "(ᵔ◡◡ᵔ)", "(✜‿‿✜)", "(♥‿‿♥)", "(☼‿‿☼)", "(≖__≖)", "(-__-)", "(╥☁╥ )", "(ب__ب)", "(☓‿‿☓)", "(#__#)"}),
+			FriendName: randomString([]string{"username", "test", "pet"}),
+
+			CPU:         0.5,
+			Memory:      0.5,
+			Temperature: 35.5,
+			Bluetooth:   randomString([]string{"-", "C", "NF"}),
+			UPS:         randomString([]string{"", "0%", "1%", "10%", "11%", "100%"}),
+
+			PeersCount: randomInt64([]int64{1, 2, 3, 10, 20, 300}),
+
+			PwnedLast:   randomString([]string{"pwnagotchi", "username", "test", "pet", "cat", "dog"}),
+			PwnedRun:    "2",
+			PwnedTotal:  randomInt64([]int64{1, 2, 3, 10, 20, 300}),
+			PwnedDeauth: "4",
+
+			TotalMessages:  randomInt64([]int64{1, 2, 3, 10, 20, 300}),
+			UnreadMessages: randomInt64([]int64{1, 2, 3, 10, 20, 300}),
+
+			Level: randomString([]string{"-", "1", "11", "50", "99"}),
+			Exp:   randomString([]string{"╷          ╷", "╷▄         ╷", "╷▄▄        ╷", "╷▄▄▄       ╷", "╷▄▄▄▄      ╷", "╷▄▄▄▄▄     ╷", "╷▄▄▄▄▄▄    ╷", "╷▄▄▄▄▄▄▄   ╷", "╷▄▄▄▄▄▄▄▄  ╷", "╷▄▄▄▄▄▄▄▄▄▄╷"}),
+		}
+
+		b, _ := json.Marshal(d)
+		_, _ = w.Write(b)
+		return
+	}
+
+	notFound := `{"initialized": false}`
+
+	if d, ok := p[hash]; ok {
+		b, err := json.Marshal(d)
+		if err != nil {
+			_, _ = w.Write([]byte(notFound))
+			return
+		}
+
+		_, _ = w.Write(b)
+		return
+	}
+
+	_, _ = w.Write([]byte(notFound))
+}
+
+func apiSet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.NotFound(w, r)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+	}
+
+	var d Pwnagotchi
+	if err := json.Unmarshal(body, &d); err != nil {
+		log.Println(err)
+	} else {
+		if d.Fingerprint != "" {
+			p[d.Fingerprint] = &d
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write([]byte(`{"success": true}`))
 }
 
 func randomInt64(list []int64) int64 {
